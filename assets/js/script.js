fac5385059a3bab5c2fe6931e1754290035fc591
maheshhost <hackingwps@gmail.com>
2019-03-17 19:25:02 +0530

    added data insert

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,6 @@ const level = {
 }
 
 const currentLevel = level.easy;
-// word
 
 //initialize game
 function init() {
@@ -40,9 +39,6 @@ function init() {
 	setInterval(checkStatus, 3000);
 }
 
-
-//TO change level
-
 //Global varaibles
 let time = currentLevel;
 let score = 0;
@@ -55,21 +51,6 @@ const scoreDisplay = document.querySelector('#score');
 const timeDisplay = document.querySelector('#time');
 const message = document.querySelector('#message');
 const seconds = document.querySelector('#seconds');
-
-// const words = ['mahesh','function','class','document.get','object','oriented','programming','inheritance','encapsulation','polymorphism',
-// 'fullstackdeveloper','frontenddeveloper','backenddeveloper','thehackernews','constructor','prototype','forloop','whileloop','switchcase','dowhileloop','decisionmakingloop','theflash','barryallen',
-// 'smart','deepubc','javaspring','charging','coffee','madperson','hacker','object.create','cr7thebest','football','codeacademy','freecodecamp','cascading','this.hacker','happyhappy','nopainnogain','timeleft','00000','reactjs','scorezero','markup','computer','wireless',
-// 'truefalse','programmer','.netdeveloper','python','javascript','whynot','speedtype','multiarray','multidimension','swapnumber','mindgames','csdojo','tictactoe','motivated','challenge','polarbear',
-// 'iamnotfast','asp.net','c#developer','comments','caitlinsnow','public static void main','helloworld','devopsdevops','ckb','ipconfig','ifconfig','bhaiparty','hackerworld',
-// 'nanobots','raspberrypi','marvenscake','youarebeautiful','abstractclass','logicprogramming','visualstudio','clientserver','arrays','youarejobless',
-// 'message','communication','public','private','protected','interview','networking','graphics','engineering','processing','loading','presentation','modelview','controller',
-// 'identifiers','authentication','authorization','httpcodes','dnsservers','domainname','worldwideweb','sublimetext','brackets','paranthesis','curlybrackets','nobackspace',
-// 'application','cookies','sessions','hypertext','error404','selflearner','nomoney','normalization','this.name','class-based','concurrent',
-// 'blockchain','tutorialspoint','thinkthink','youarethebest','parentclass','childclass','instanceof','iterators','properties','methods','setattribute','getattribute',
-// 'globalscope','localscope','function()','typeof','string','namespace','interface','readonly','override','const','explicit','delegate','continue','malloc','stackalloc','foreach',
-// 'math.random','information'
-// ];
-// url :- https://api.myjson.com/bins/1h9kx2
 
 function matchWords(){
 	wordInput.value = wordInput.value.toLowerCase();
